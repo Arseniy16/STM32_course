0c9266c827b7fa109e6af31fff9de085d7d63194
Arseniy16 <arseniyi16.11@gmail.com>
2021-04-19 00:05:31 +0300

    change

--- MAIN_STM/Lab08/Lab_08_02/08_timers_pwm/main.c
+++ MAIN_STM/Lab08/Lab_08_02/08_timers_pwm/main.c
@@ -51,68 +51,68 @@ uint32_t mask_indicator(uint32_t mask)
   */
 static void rcc_config()
 {
-  /* Set FLASH latency */
-  LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
+	/* Set FLASH latency */
+	LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
 
-  /* Enable HSI and wait for activation*/
-  LL_RCC_HSI_Enable();
-  while (LL_RCC_HSI_IsReady() != 1);
+	/* Enable HSI and wait for activation*/
+	LL_RCC_HSI_Enable();
+	while (LL_RCC_HSI_IsReady() != 1);
 
-  /* Main PLL configuration and activation */
-  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI_DIV_2,
+	/* Main PLL configuration and activation */
+	LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI_DIV_2,
 				LL_RCC_PLL_MUL_12);
 
-  LL_RCC_PLL_Enable();
-  while (LL_RCC_PLL_IsReady() != 1);
+	LL_RCC_PLL_Enable();
+	while (LL_RCC_PLL_IsReady() != 1);
 
-  /* Sysclk activation on the main PLL */
-  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
-  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
-  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
+	/* Sysclk activation on the main PLL */
+	LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+	LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+	while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
 
-  /* Set APB1 prescaler */
-  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+	/* Set APB1 prescaler */
+	LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
 
-  /* Update CMSIS variable (which can be updated also
-   * through SystemCoreClockUpdate function) */
-  SystemCoreClock = 48000000;
+	/* Update CMSIS variable (which can be updated also
+	 * through SystemCoreClockUpdate function) */
+	SystemCoreClock = 48000000;
 }
 /*---------------------------------------------*/
 static void gpio_config(void)
 {
-  /*
-   * Init two default LEDs
-   */
-  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
-  LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_8, LL_GPIO_MODE_OUTPUT);
-  LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_9, LL_GPIO_MODE_OUTPUT);
-  /*
-   * Init main ports for indicators
-   * from MSB to LSB -> 3_2_1_0    
-  */
-  LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_0, LL_GPIO_MODE_OUTPUT);
-  LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_1, LL_GPIO_MODE_OUTPUT);
-  LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_2, LL_GPIO_MODE_OUTPUT);
-  LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_3, LL_GPIO_MODE_OUTPUT);
-  /*
-   * Init port for USER button
-   */
-  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
-  LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_0, LL_GPIO_MODE_INPUT);
-  /*
-   * Init ports for indicator
-   */
-  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
-  LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_0, LL_GPIO_MODE_OUTPUT);
-  LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_1, LL_GPIO_MODE_OUTPUT);
-  LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_2, LL_GPIO_MODE_OUTPUT);
-  LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_3, LL_GPIO_MODE_OUTPUT);
-  LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_4, LL_GPIO_MODE_OUTPUT);
-  LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_5, LL_GPIO_MODE_OUTPUT);
-  LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_6, LL_GPIO_MODE_OUTPUT);
-  LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_7, LL_GPIO_MODE_OUTPUT);
+	/*
+	* Init two default LEDs
+	*/
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_8, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_9, LL_GPIO_MODE_OUTPUT);
+	/*
+	* Init main ports for indicators
+	* from MSB to LSB -> 3_2_1_0    
+	*/
+	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_0, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_1, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_2, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_3, LL_GPIO_MODE_OUTPUT);
+	/*
+	* Init port for USER button
+	*/
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+	LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_0, LL_GPIO_MODE_INPUT);
+	/*
+	* Init ports for indicator
+	*/
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_0, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_1, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_2, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_3, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_4, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_5, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_6, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_7, LL_GPIO_MODE_OUTPUT);
 
-  return;
+	return;
 }
 /*---------------------------------------------*/
 /*
@@ -120,11 +120,11 @@ static void gpio_config(void)
  */
 void dec_display(uint32_t number)
 {
-  static uint32_t digit_num = 0;
-  uint16_t out = 0;
+	static uint32_t digit_num = 0;
+	uint16_t out = 0;
 
-  const uint16_t decoder[] = 
-  {
+	const uint16_t decoder[] = 
+	{
 	bits(0,0,1,1,1,1,1,1), //0
 	bits(0,0,0,0,0,1,1,0), //1
 	bits(0,1,0,1,1,0,1,1), //2
@@ -135,22 +135,24 @@ void dec_display(uint32_t number)
 	bits(0,0,0,0,0,1,1,1), //7
 	bits(0,1,1,1,1,1,1,1), //8
 	bits(0,1,1,0,1,1,1,1), //9
-  }; 
+	}; 
 
-  //update indicator
-  LL_GPIO_WriteOutputPort(GPIOC, mask_indicator(0b1111));
-  
-  //turn on a particular indicator
-  LL_GPIO_ResetOutputPin(GPIOC, mask_indicator(1<<digit_num));
+	//update indicator
+	LL_GPIO_WriteOutputPort(GPIOC, mask_indicator(0b1111));
 
-  if(digit_num == 0) out = decoder[(number % 10)];
-  if(digit_num == 1) out = decoder[(number / 10) % 10];
-  if(digit_num == 2) out = decoder[(number / 100) % 10];
-  if(digit_num == 3) out = decoder[(number / 1000) % 10];
+	//turn on a particular indicator
+	LL_GPIO_ResetOutputPin(GPIOC, mask_indicator(1<<digit_num));
 
-  LL_GPIO_WriteOutputPort(GPIOB, mask_indicator(out));
+	if(digit_num == 0) out = decoder[(number % 10)];
+	if(digit_num == 1) out = decoder[(number / 10) % 10];
+	if(digit_num == 2) out = decoder[(number / 100) % 10];
+	if(digit_num == 3) out = decoder[(number / 1000) % 10];
 
-  digit_num = (digit_num + 1) % 4;
+	LL_GPIO_WriteOutputPort(GPIOB, mask_indicator(out));
+
+	digit_num = (digit_num + 1) % 4;
+	
+	return;	
 }
 /*---------------------------------------------*/
 /*
